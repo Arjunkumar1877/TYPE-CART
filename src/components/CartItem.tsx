46313a9baa030fe3dc2d8423c851aae530da4564
Arjun kumar vs <arjun.tech177@gmail.com>
2024-05-24 13:59:37 +0530

    cart updation done

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -1,6 +1,7 @@
-import { Stack } from "react-bootstrap";
+import { Button, Stack } from "react-bootstrap";
 import { useShoppingCart } from "../context/ShoppingCartContext";
 import StoreItem from "../data/items.json";
+import { formatCurrency } from "../utilities/formatCurrency";
 
 type CartItemPros = {
     id: number;
@@ -14,8 +15,18 @@ export function CartItem({id, quantity}: CartItemPros){
  if(item == null) return null
 
  return (
-    <Stack direction="horizontal" gap={2} >
-        <img src={item.imgUrl} />
+    <Stack direction="horizontal" gap={2} className="d-flex align-items-center">
+        <img src={item.imgUrl} style={{width: "125px", height: "75px", objectFit: "cover"}} />
+        <div className="me-auto">
+            <div className="">
+                {item.name} {quantity > 1 && (
+                    <span className="text-muted" style={{fontSize: ".65rem"}}>{quantity} x</span>
+                )} 
+            </div>
+            <div className="text-muted" style={{fontSize: ".75rem"}}>{formatCurrency(item.price)}</div>
+        </div>
+        <div className="">{formatCurrency(item.price * quantity)}</div>
+        <Button variant="outline-danger" onClick={()=> removeFromCart(item.id)}>&times;</Button>
     </Stack>
  )
 
--- src/components/ShoppingCart.tsx
+++ src/components/ShoppingCart.tsx
@@ -1,6 +1,8 @@
 import { Offcanvas, OffcanvasBody, OffcanvasHeader, OffcanvasTitle, Stack } from "react-bootstrap";
 import { useShoppingCart } from "../context/ShoppingCartContext";
 import { CartItem } from "./CartItem";
+import { formatCurrency } from "../utilities/formatCurrency";
+import StoreItems from "../data/items.json";
 
 type ShoppingCartProps = {
     isOpen: boolean
@@ -18,6 +20,13 @@ export function ShoppingCart({ isOpen }: ShoppingCartProps){
                 <CartItem key={item.id} {...item} />
               ))
               }
+
+              <div className="ms-auto fw-bold fs-5">
+                 Total {" "} {formatCurrency(cartItems.reduce((total, cartItem)=> {
+                    const item = StoreItems.find(i => i.id === cartItem.id);
+                    return total + (item?.price || 0) * cartItem.quantity
+                 }, 0))}
+              </div>
             </Stack>
         </Offcanvas.Body>
     </Offcanvas>
