64ca8aaf4024f42c978a36298e43b67795897fba
Arjun kumar vs <arjun.tech177@gmail.com>
2024-05-24 05:38:43 +0530

    Rebuilt the index file

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import StoreItem from '../components/StoreItem'
+import storeItems from '../data/items.json'
+import { Col, Row } from 'react-bootstrap'
 
 const Store = () => {
   return (
-    <div>
-      Store
-    </div>
+   <>
+   <h1>Store</h1>
+   <Row md={2} xs={1} lg={3} className='g-3'>
+    <Col>
+    {
+      storeItems.map(item => (
+        <StoreItem {...item} />
+      ))
+    }
+    </Col>
+   </Row>
+   </>
   )
 }
 
